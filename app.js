db1a73d38fb2c35ef65a07d4d308b2472de53d7b
sugijit <b.suldsaikhan@gmail.com>
2023-08-18 22:51:24 +0900

    added sound

--- app.js
+++ app.js
@@ -14,145 +14,162 @@ document.addEventListener("DOMContentLoaded", function () {
     //tsag
     switch (hour1) {
       case 1:
-        document.getElementById("9").classList.remove("now");
-        document.getElementById("20").classList.remove("now");
-        document.getElementById("10").classList.add("now");
+        document.getElementById("9").classList.remove("on");
+        document.getElementById("20").classList.remove("on");
+        document.getElementById("10").classList.add("on");
         break;
       case 2:
-        document.getElementById("9").classList.remove("now");
-        document.getElementById("10").classList.remove("now");
-        document.getElementById("20").classList.add("now");
+        document.getElementById("9").classList.remove("on");
+        document.getElementById("10").classList.remove("on");
+        document.getElementById("20").classList.add("on");
         break;
     }
     switch (hour2) {
       case 1:
-        document.getElementById("9").classList.remove("now");
+        document.getElementById("9").classList.remove("on");
         if (hour1 === 0) {
-          document.getElementById("1").classList.add("now");
+          document.getElementById("1").classList.add("on");
         } else {
-          document.getElementById("negen").classList.add("now");
+          document.getElementById("negen").classList.add("on");
         }
         break;
       case 2:
-        document.getElementById("1").classList.remove("now");
-        document.getElementById("negen").classList.remove("now");
-        document.getElementById("2").classList.add("now");
+        document.getElementById("1").classList.remove("on");
+        document.getElementById("negen").classList.remove("on");
+        document.getElementById("2").classList.add("on");
         break;
       case 3:
-        document.getElementById("2").classList.remove("now");
-        document.getElementById("3").classList.add("now");
+        document.getElementById("2").classList.remove("on");
+        document.getElementById("3").classList.add("on");
         break;
       case 4:
-        document.getElementById("3").classList.remove("now");
-        document.getElementById("4").classList.add("now");
+        document.getElementById("3").classList.remove("on");
+        document.getElementById("4").classList.add("on");
         break;
       case 5:
-        document.getElementById("4").classList.remove("now");
-        document.getElementById("5").classList.add("now");
+        document.getElementById("4").classList.remove("on");
+        document.getElementById("5").classList.add("on");
         break;
       case 6:
-        document.getElementById("5").classList.remove("now");
-        document.getElementById("6").classList.add("now");
+        document.getElementById("5").classList.remove("on");
+        document.getElementById("6").classList.add("on");
         break;
       case 7:
-        document.getElementById("6").classList.remove("now");
-        document.getElementById("7").classList.add("now");
+        document.getElementById("6").classList.remove("on");
+        document.getElementById("7").classList.add("on");
         break;
       case 8:
-        document.getElementById("7").classList.remove("now");
-        document.getElementById("8").classList.add("now");
+        document.getElementById("7").classList.remove("on");
+        document.getElementById("8").classList.add("on");
         break;
       case 9:
-        document.getElementById("8").classList.remove("now");
-        document.getElementById("9").classList.add("now");
+        document.getElementById("8").classList.remove("on");
+        document.getElementById("9").classList.add("on");
         break;
     }
     if (hour1 === 0 && hour2 === 0) {
-      document.getElementById("horin").classList.remove("now");
-      document.getElementById("gurvan").classList.remove("now");
-      document.getElementById("9").classList.remove("now");
-      document.getElementById("0").classList.add("now");
+      document.getElementById("horin").classList.remove("on");
+      document.getElementById("gurvan").classList.remove("on");
+      document.getElementById("9").classList.remove("on");
+      document.getElementById("0").classList.add("on");
     } else {
-      document.getElementById("0").classList.remove("now");
+      document.getElementById("0").classList.remove("on");
     }
 
     //minut
     switch (min1) {
       case 1:
-        document.getElementById("tavin").classList.remove("now");
-        document.getElementById("yusun").classList.remove("now");
-        document.getElementById("arvan").classList.add("now");
+        document.getElementById("tavin").classList.remove("on");
+        document.getElementById("yusun").classList.remove("on");
+        document.getElementById("arvan").classList.add("on");
         break;
       case 2:
-        document.getElementById("arvan").classList.remove("now");
-        document.getElementById("yusun").classList.remove("now");
-        document.getElementById("horin").classList.add("now");
+        document.getElementById("arvan").classList.remove("on");
+        document.getElementById("yusun").classList.remove("on");
+        document.getElementById("horin").classList.add("on");
         break;
       case 3:
-        document.getElementById("horin").classList.remove("now");
-        document.getElementById("yusun").classList.remove("now");
-        document.getElementById("guchin").classList.add("now");
+        document.getElementById("horin").classList.remove("on");
+        document.getElementById("yusun").classList.remove("on");
+        document.getElementById("guchin").classList.add("on");
         break;
       case 4:
-        document.getElementById("guchin").classList.remove("now");
-        document.getElementById("yusun").classList.remove("now");
-        document.getElementById("duchin").classList.add("now");
+        document.getElementById("guchin").classList.remove("on");
+        document.getElementById("yusun").classList.remove("on");
+        document.getElementById("duchin").classList.add("on");
         break;
       case 5:
-        document.getElementById("duchin").classList.remove("now");
-        document.getElementById("yusun").classList.remove("now");
-        document.getElementById("tavin").classList.add("now");
+        document.getElementById("duchin").classList.remove("on");
+        document.getElementById("yusun").classList.remove("on");
+        document.getElementById("tavin").classList.add("on");
         break;
     }
     switch (min2) {
       case 1:
-        document.getElementById("yusun").classList.remove("now");
+        document.getElementById("yusun").classList.remove("on");
         if (min1 === 0) {
-          document.getElementById("neg").classList.add("now");
+          document.getElementById("neg").classList.add("on");
         } else {
-          document.getElementById("negenn").classList.add("now");
+          document.getElementById("negenn").classList.add("on");
         }
         break;
       case 2:
-        document.getElementById("negenn").classList.remove("now");
-        document.getElementById("neg").classList.remove("now");
-        document.getElementById("hoyor").classList.add("now");
+        document.getElementById("negenn").classList.remove("on");
+        document.getElementById("neg").classList.remove("on");
+        document.getElementById("hoyor").classList.add("on");
         break;
       case 3:
-        document.getElementById("hoyor").classList.remove("now");
-        document.getElementById("gurvan").classList.add("now");
+        document.getElementById("hoyor").classList.remove("on");
+        document.getElementById("gurvan").classList.add("on");
         break;
       case 4:
-        document.getElementById("gurvan").classList.remove("now");
-        document.getElementById("durvun").classList.add("now");
+        document.getElementById("gurvan").classList.remove("on");
+        document.getElementById("durvun").classList.add("on");
         break;
       case 5:
-        document.getElementById("durvun").classList.remove("now");
-        document.getElementById("tavan").classList.add("now");
+        document.getElementById("durvun").classList.remove("on");
+        document.getElementById("tavan").classList.add("on");
         break;
       case 6:
-        document.getElementById("tavan").classList.remove("now");
-        document.getElementById("zurgaan").classList.add("now");
+        document.getElementById("tavan").classList.remove("on");
+        document.getElementById("zurgaan").classList.add("on");
         break;
       case 7:
-        document.getElementById("zurgaan").classList.remove("now");
-        document.getElementById("doloon").classList.add("now");
+        document.getElementById("zurgaan").classList.remove("on");
+        document.getElementById("doloon").classList.add("on");
         break;
       case 8:
-        document.getElementById("doloon").classList.remove("now");
-        document.getElementById("naiman").classList.add("now");
+        document.getElementById("doloon").classList.remove("on");
+        document.getElementById("naiman").classList.add("on");
         break;
       case 9:
-        document.getElementById("naiman").classList.remove("now");
-        document.getElementById("yusun").classList.add("now");
+        document.getElementById("naiman").classList.remove("on");
+        document.getElementById("yusun").classList.add("on");
         break;
     }
     if (min1 === 0 && min2 === 0) {
-      document.getElementById("tavin").classList.remove("now");
-      document.getElementById("yusun").classList.remove("now");
-      document.getElementById("teg").classList.add("now");
+      document.getElementById("tavin").classList.remove("on");
+      document.getElementById("yusun").classList.remove("on");
+      document.getElementById("teg").classList.add("on");
     } else {
-      document.getElementById("teg").classList.remove("now");
+      document.getElementById("teg").classList.remove("on");
     }
   }
+});
+
+const soundSpans1 = document.querySelectorAll(".sheep1");
+const soundSpans2 = document.querySelectorAll(".sheep2");
+const audioElement1 = document.getElementById("audioElement1");
+const audioElement2 = document.getElementById("audioElement2");
+
+soundSpans1.forEach((span) => {
+  span.addEventListener("click", () => {
+    audioElement1.play();
+  });
+});
+
+soundSpans2.forEach((span) => {
+  span.addEventListener("click", () => {
+    audioElement2.play();
+  });
 });
